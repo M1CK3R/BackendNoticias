d1006e2e1818e6a6b1590b11c1fa3892825ff760
Erick Carbonell <23003931@galileo.edu>
2024-10-14 22:37:10 -0600

    V:0.1 Backend solo con Contactos

--- src/contacts/services/contacts.service.ts
+++ src/contacts/services/contacts.service.ts
@@ -7,16 +7,16 @@ export interface Contact {
   phone: string;
   carrera: string;
   puesto: string;
-  imagen: TexImageSource;
+  imagen: string;
 }
 
 @Injectable()
 export class ContactsService {
   private contacts: Contact[] = [
-    { id: 1, name: 'John Doe', email: 'john@example.com', phone: '123456789',  },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com', phone: '987654321', },
-    { id: 3, name: 'Mike Whithe', email: 'mwhithe@example.com', phone: '8765466757' },
-    { id: 4, name: 'Jess Sand', email: 'jsand@example.com', phone: '987865467' }
+    { id: 1, name: 'John Doe', email: 'john@example.com', phone: '123456789', carrera: 'Tecnico en Desarrollo de Software', puesto: 'Auxiliar', imagen: '***', },
+    { id: 2, name: 'Jane Smith', email: 'jane@example.com', phone: '987654321', carrera: 'Tecnico en Desarrollo de Software', puesto: 'Auxiliar', imagen: '***' },
+    { id: 3, name: 'Mike Whithe', email: 'mwhithe@example.com', phone: '8765466757', carrera: 'Tecnico en Desarrollo de Software', puesto: 'Auxiliar', imagen: '***' },
+    { id: 4, name: 'Jess Sand', email: 'jsand@example.com', phone: '987865467', carrera: 'Tecnico en Desarrollo de Software', puesto: 'Auxiliar', imagen: '**' }
     // Agrega más contactos simulados aquí
   ];
 
